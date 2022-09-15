b89e2c1be9195e18ea82f30d7ed72d121e55a880
jinyangp <jinyangpeh@gmail.com>
2022-09-15 12:05:38 +0800

    Updated port number

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
 const app = express();
+const port = 8080;
 
-app.listen(5000, () => {
-  console.log("Listening on port 5000");
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
 });
