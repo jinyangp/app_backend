1f3c8858899dfc6003aa6a089a9fc034615900a8
jinyangp <jinyangpeh@gmail.com>
2022-09-25 19:40:52 +0800

    included database field

--- middlewares/dbconfig.js
+++ middlewares/dbconfig.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
-const { DB_HOSTNAME, DB_USER, DB_PW } = require("../config");
+const { DB_HOSTNAME, DB_USER, DB_PW, DB_NAME } = require("../config");
 
 const dbconnect = {
   getConnection: mysql.createConnection({
     host: DB_HOSTNAME,
     user: DB_USER,
     password: DB_USER,
+    database: DB_NAME,
   }),
 };
 
